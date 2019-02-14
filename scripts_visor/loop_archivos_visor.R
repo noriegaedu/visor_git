f4cdfbbf0f893fc0e86e552bbd4f65665e27c33a
noriegaedu <eduardonoriegac@gmail.com>
2019-02-14 10:25:45 -0400

    se movieroin vairables a archivo source y se anhadieron varaibles para nuevas opciones de funcion generadora de archivos visor

--- scripts_visor/loop_archivos_visor.R
+++ scripts_visor/loop_archivos_visor.R
@@ -1,5 +1,5 @@
-variables <- c('BF', 'ETP', 'ETR', 'IN', 'PCP', 'SR')
 
+# BHSB2017
 shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/WEAPCatchment.shp"
 # usar el del modelo weap #shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
 #shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
@@ -9,18 +9,18 @@ salida <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug20
 salida_shp <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/bh/shp/"
 
 
-for (i in variables) {
-    arch_visor(shp_cuencas, var_csv, i, salida, salida_shp)
-}
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo=TRUE)
 
-# para escurriemiento
-lis_esc <- list.files(salida, pattern = 'visor', full.names = TRUE) %>% 
-    grep('BF|IN|SR', ., value = TRUE) %>% 
-    lapply(function(x) read.csv(x, stringsAsFactors = FALSE)) %>% 
-    Reduce('+', .)
+# Ravelo
+shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/Cuenca_Cachimayu_UH_weap.shp"
+# usar el del modelo weap #shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+#shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+var_csv <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/bh/"
+#abr_var <- 'PCP'
+salida <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/bh/"
+salida_shp <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/bh/shp/"
+corregir_ah <- FALSE
+escribir_shp <- FALSE
 
-lis_esc[,1:2] <- lis_esc[,1:2]/3L
 
-write.csv(lis_esc, paste0(salida, 'esc_visor.csv'), row.names = FALSE)
-
-
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo=TRUE)
