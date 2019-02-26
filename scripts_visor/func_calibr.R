22ed5aef03a0e59d91ab239ba644f4b7f4e000aa
noriegaedu <eduardonoriegac@gmail.com>
2019-02-26 09:05:55 -0400

    unido con funcion csv_q_visor

--- scripts_visor/func_calibr.R
+++ scripts_visor/func_calibr.R
@@ -1,7 +1,9 @@
 #' @author Eduardo Noriega
 #' 
 #' @description La funcion permite crear un shapefile de calibracion en base a un shapefile de reporte
-#' de un balance hidrico
+#' de un balance hidrico y genera los archivos para el geovisor de las todas las variables (ETP, ETR, PCP, 
+#' BF, IN, SR, Q_sim y Q_obs). Si es necesario la funcion corrige el desfase temporal 
+#' de los archivos de caudal.
 #' 
 #' @param datos_resumen csv con la siguiente estructura (sin nombre de columnas)
 #' |-a-|-b-|-c-|-d-|
@@ -14,7 +16,9 @@
 #' d = Existe estacion hidrometrica para nodo del que se extrae informacion? Binario (1 o 0)
 #' @param shp_reporte ruta del shapefile de reporte de la cuenca (el que fue usado para visor)
 #' @param shp_calibr ruta para escribir shapefile de calibracion
-#' 
+#' @param ruta_q ruta donde se enceuntran los archivos de caudal de WEAP
+#' @param corregir_ah logico, TRUE para corregir fechas por anho hidorlogico
+#'  
 #' @details dentro de la estructura de la funcion se presenta una funcion adicional llamada 'uh_calibr'
 #' esta funcion permite asociar la UH del reporte con las UH de la calibracion de forma interactiva
 #' al ser llamada, la funcion itera a traves de la cantidad de datos (filas, uh de calibracion) del archivo
@@ -31,10 +35,13 @@
 #'        "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/bh/shp/ravelo_calibr_solo2.shp",
 #'        reporte = TRUE,
 #'        ruta_entrada_reportes = "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/bh/", 
-#'        ruta_salida_reportes = "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/bh/")
+#'        ruta_salida_reportes = "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/bh/",
+#'        ruta_q = 'C:/Users/HP/Documents/WEAP Areas/BH Azero/bh/',
+#'        corregir_ah = TRUE)
 
-calibr <- function(datos_resumen, shp_reporte, shp_calibr, 
-                   reportes = FALSE, ruta_entrada_reportes, ruta_salida_reportes){
+calibr <- function(datos_resumen, shp_reporte, shp_calibr, reportes = FALSE, 
+                   ruta_entrada_reportes, ruta_salida_reportes, 
+                   ruta_q, corregir_ah = FALSE){
     
     require(tidyverse)
     require(sf)
@@ -44,7 +51,7 @@ calibr <- function(datos_resumen, shp_reporte, shp_calibr,
                                  stringsAsFactors = FALSE) %>% 
         pull(1)
     
-    shp_reporte <- st_read(shp_reporte)
+    shp_reporte <- st_read(shp_reporte, quiet = TRUE)
     
     #source('H:/mmaya/proyectos_R/visor_git/scripts_visor/func_asign_uh.R') # funcion de asignacion uh reporte a uh calibracion
     uh_calibr <- function(){
@@ -73,6 +80,8 @@ calibr <- function(datos_resumen, shp_reporte, shp_calibr,
                   codigo = paste0('uhc', seq_along(nombres_features))) %>% 
         st_write(shp_calibr)
     
+    # otras varaibles difenrtes de Q
+    
     if (reportes == TRUE) {
         reportes_csv <- list.files(ruta_entrada_reportes, 
                                    pattern = 'visor', full.names = TRUE) %>% 
@@ -97,5 +106,50 @@ calibr <- function(datos_resumen, shp_reporte, shp_calibr,
                           names(reportes_calibr), 
                           '_calibr.csv'))
         }
+    
+    # para caudal
+    qs <- list.files(ruta_q,
+                     pattern = 'Q_', full.names = TRUE) %>% 
+        lapply(read.csv, check.names = FALSE)
+    qs_names <- list.files(ruta_q,
+                           pattern = 'Q_', full.names = TRUE) %>% gsub('.csv', '', .)
+    
+    if (isFALSE(corregir_ah)) {
+        fechas <- lapply(qs, function(x) dplyr::select(x, 2:3)) %>% 
+            lapply(function(x) setNames(x, c('Year', 'Month')))
+    }
+    
+    if (isTRUE(corregir_ah)) {
+        fechas <- lapply(qs, function(x) dplyr::select(x, 2:3)) %>% 
+            lapply(function(x) setNames(x, c('Year', 'Month')))
         
+        anhos <- fechas[[1]]$Year %>% unique()
+        ini <- anhos %>% head(1)
+        fin <- anhos %>% tail(1)
+        
+        fechas <- lapply(qs, function(x) dplyr::select(x, 2:3)) %>% 
+            lapply(function(x) setNames(x, c('Year', 'Month'))) %>% 
+            lapply(function(x) mutate(x, Year = c(rep(ini - 1,4), 
+                                                  rep(seq(ini, fin - 1), each = 12), 
+                                                  rep(fin,8)),
+                                      Month = rep(c(9:12,1:8), length(anhos))))
+        
+        # corregir salida de WEAP
+        qs_orig <- qs %>% 
+            lapply(function(x) mutate(x, Year = fechas[[1]]$Year,
+                                      TS = fechas[[1]]$Month))
+        mapply(function(x, y) write.csv(x, y, row.names = FALSE), 
+               x = qs_orig, y = paste0(qs_names, '.csv'))
+        
+    }
+    
+    datos <- lapply(qs, function(x) dplyr::select(x, -c(1:3))) %>% 
+        lapply(function(x) replace(x, x == -999, '')) %>% 
+        lapply(function(x) setNames(x, paste0('uhc', seq_along(qs))))
+    
+    csv <- lapply(seq_along(qs), function(x) cbind(fechas[[x]], datos[[x]]))
+    
+    mapply(function(x, y) write.csv(x, y, row.names = FALSE), 
+           x = csv, y = paste0(qs_names, '_visor.csv'))
+    
 }
