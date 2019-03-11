fd8c468a3be977cdc6af56e9c0f3d3c95f88a78b
noriegaedu <eduardonoriegac@gmail.com>
2019-03-11 11:39:02 -0400

    anhadidos algunos bh

--- scripts_visor/loop_archivos_visor.R
+++ scripts_visor/loop_archivos_visor.R
@@ -1,3 +1,4 @@
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/func_arch_visor.R', echo = TRUE)
 
 # BHSB2017
 shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/WEAPCatchment.shp"
@@ -7,9 +8,11 @@ var_csv <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2
 #abr_var <- 'PCP'
 salida <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/bh/"
 salida_shp <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/bh/shp/"
+corregir_ah <- FALSE
+escribir_shp <- TRUE
 
 
-source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo=TRUE)
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo = TRUE)
 
 # Ravelo
 shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Ravelo FINAL/Cuenca_Cachimayu_UH_weap.shp"
@@ -24,3 +27,55 @@ escribir_shp <- FALSE
 
 
 source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo=TRUE)
+
+
+# Laguna COlorada
+shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/LagunaColorada FINAL/subcuencas_UH_weap.shp"
+# usar el del modelo weap #shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+#shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+var_csv <- "C:/Users/HP/Documents/WEAP Areas/LagunaColorada FINAL/bh/"
+#abr_var <- 'PCP'
+salida <- "C:/Users/HP/Documents/WEAP Areas/LagunaColorada FINAL/bh/"
+salida_shp <- "C:/Users/HP/Documents/WEAP Areas/LagunaColorada FINAL/bh/shp/"
+corregir_ah <- FALSE
+escribir_shp <- TRUE
+
+
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo = TRUE)
+
+# Azero
+shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BH Azero/subcuencas_UH_weap.shp"
+# usar el del modelo weap #shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+#shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+var_csv <- "C:/Users/HP/Documents/WEAP Areas/BH Azero/bh/"
+#abr_var <- 'PCP'
+salida <- "C:/Users/HP/Documents/WEAP Areas/BH Azero/bh/"
+salida_shp <- "C:/Users/HP/Documents/WEAP Areas/BH Azero/bh/shp/"
+corregir_ah <- TRUE
+escribir_shp <- TRUE
+
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo = TRUE)
+
+# Guadalquivir
+shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Guadalquivir v1.2/subcuencas_UH_weap_4.shp"
+# usar el del modelo weap #shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+#shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Amazonica_Aug2018/Calibracion/SIG/CatchmentsNacional.shp"
+var_csv <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Guadalquivir v1.2/bh/"
+#abr_var <- 'PCP'
+salida <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Guadalquivir v1.2/bh/"
+salida_shp <- "C:/Users/HP/Documents/WEAP Areas/Balance Hidrico Guadalquivir v1.2/bh/shp/"
+corregir_ah <- FALSE
+escribir_shp <- TRUE
+
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo = TRUE)
+
+# Cerrada_Plata
+shp_cuencas <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Cerrada_Plata_Aug2018/WEAPCatchment.shp"
+var_csv <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Cerrada_Plata_Aug2018/bh/"
+#abr_var <- 'PCP'
+salida <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Cerrada_Plata_Aug2018/bh/"
+salida_shp <- "C:/Users/HP/Documents/WEAP Areas/BHSB_Modelo Nacional_Cerrada_Plata_Aug2018/bh/shp/"
+corregir_ah <- FALSE
+escribir_shp <- TRUE
+
+source('H:/mmaya/proyectos_R/visor_git/scripts_visor/loop_archivos_visor_codigo.R', echo = TRUE)
